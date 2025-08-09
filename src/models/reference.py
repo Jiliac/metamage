7f765617c5c325ff0089e93ed5f6b87c5eeab1f2
Valentin Manes <valentinmanes@outlook.fr>
2025-08-09 13:45:32 +0200

    feat: bans and sets

--- src/models/reference.py
+++ src/models/reference.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Text, Index
+from sqlalchemy import Column, String, DateTime, ForeignKey, Text, Index
 from sqlalchemy.orm import relationship
 from sqlalchemy.types import TypeDecorator, VARCHAR
 from .base import Base, uuid_pk
